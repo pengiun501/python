07fdeb75c9af365579b3f00e53f0ebe6b0afd05f
pengiun501 <70123706+pengiun501@users.noreply.github.com>
2020-08-24 20:12:51 +0800

    Add files via upload

---
+++ 分數.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 19:59:25 2020
+
+@author: Alice
+"""
+
+score=int(input('please input your score:'))
+if score>=90:
+    print("Hello, I'm an A++ T.Rex!!!")
+elif score>=80:
+    print("Hello, I'm a B++ T.Rex!!!")
+elif score>=70:
+    print("Hello, I'm a C++ T.Rex!!!")
+elif score>=60:
+    print("Hello, I'm a D++ T.Rex!!!")
+else:
+    print("Hello, I'm an A++ slug!!!")
---
+++ 哈囉.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 20:11:37 2020
+
+@author: Alice
+"""
+
+print("Hello, I'm a T.Rex!!!")
+name=input("what's your name?")
+print("Hi,",name)
