f3781261b4e0b8923a655c749b3e890c54e2cd1a
pengiun501 <70123706+pengiun501@users.noreply.github.com>
2020-08-24 15:00:09 +0800

    Add files via upload

---
+++ 成績.py
@@ -0,0 +1,5 @@
+score=int(input('please input score:'))
+if score<60:
+    print('fail')
+else:
+    print('pass')
---
+++ 條件判斷式.py
@@ -0,0 +1,5 @@
+score=int(input('please input your score:'))
+if score<60:
+    print('pass')
+else:
+    print('pass')
