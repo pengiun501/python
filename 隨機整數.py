e43fbc685349d883a1bdb9c7d28456dac0fd1c0c
pengiun501 <70123706+pengiun501@users.noreply.github.com>
2020-08-25 14:03:37 +0800

    Add files via upload

---
+++ 隨機整數.py
@@ -0,0 +1,4 @@
+import random
+num=random.randint(10,60)
+print(num)
+                                                                                                                                                                                                                                                                                                                                                                                                                                     
