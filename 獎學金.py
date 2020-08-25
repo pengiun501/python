099a901d6ec8a381be164de70784cd3482f515dc
pengiun501 <70123706+pengiun501@users.noreply.github.com>
2020-08-25 10:38:47 +0800

    Add files via upload

--- 獎學金.py
+++ 獎學金.py
@@ -1,4 +1,4 @@
-math=int(input('plesae input math score:'))
-eng=int(input('plesae input english score:'))
+math=int(input('please input math score:'))
+eng=int(input('please input english score:'))
 if (math>=90 and eng>=90) or (math==100 or eng==100):
     print('恭喜得到獎學金')
